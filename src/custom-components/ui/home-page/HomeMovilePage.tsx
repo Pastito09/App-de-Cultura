97178604ec9c2c08d63bdd5c2b6146e117150222
Manuel Salvadores <patriciozhogan@gmail.com>
2025-02-26 19:23:55 -0300

    miercoles 26/6

--- src/custom-components/ui/home-page/HomeMovilePage.tsx
+++ src/custom-components/ui/home-page/HomeMovilePage.tsx
@@ -1,5 +1,6 @@
 import { CalendarGrid } from '@/custom-components/home-page/CalendarGrid';
 import { EventsGrid } from '@/custom-components/home-page/EventsGrid';
+import { Footer } from '../Footer';
 // import { Sidebar } from '@/custom-components/home-page/Sidebar';
 
 export const HomeMovilePage = () => {
@@ -15,6 +16,9 @@ export const HomeMovilePage = () => {
       {/* <div className='lg:w-[12%] h-screen border hidden lg:block sm:order-3 ms-1 border-slate-300 rounded-lg'>
         <Sidebar />
       </div> */}
+      <div>
+        <Footer />
+      </div>
     </div>
   );
 };
