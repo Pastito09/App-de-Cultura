9300a8590be478bcce6e0a458154c049cc71dd0d
Manuel Salvadores <patriciozhogan@gmail.com>
2025-02-26 18:47:49 -0300

    miercoles 26/2

--- src/app/(home)/[slug]/page.tsx
+++ src/app/(home)/[slug]/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import { initialData } from '../../../seed/seed';
 import EventPage from '@/custom-components/events-page/EventPage';
-import { Footer } from '@/custom-components';
-import Link from 'next/link';
-interface Props {
-  params: { slug: string };
-}
+
 const data = initialData.events;
-export default async function EventoPage({ params }: Props) {
+export default async function EventoPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   let { slug } = await params;
 
   const evento = data.find(({ id }) => id === slug);
