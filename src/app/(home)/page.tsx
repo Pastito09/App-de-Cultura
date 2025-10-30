e325bc9f96296c1c40de5dd5530a0f9f90fb9a49
Manuel Salvadores <patriciozhogan@gmail.com>
2025-10-30 19:48:37 -0300

    resuelto el corte de dias

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,12 +4,13 @@ import {
   getAllCalendarEvents,
   getAllEventsWithImage,
 } from '@/actions';
+import { getPaginatedEvents } from '@/actions/events/getPaginatedEvents';
 import { HomeMovilePage } from '@/custom-components/ui/home-page/HomeMovilePage';
 import { HomePage } from '@/custom-components/ui/home-page/HomePage';
 
 export default async function Home() {
   const { events: calendarEvent } = await getAllCalendarEvents();
-  const { events } = await getAllEventsWithImage();
+  const { events } = await getPaginatedEvents({ page: 1, take: 50 });
   return (
     <>
       <div className='hidden lg:block'>
