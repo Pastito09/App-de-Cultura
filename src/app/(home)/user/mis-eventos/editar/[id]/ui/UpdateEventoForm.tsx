a4cc47c673e3cefbcab49d5c6ebb3f94a8d9099b
Manuel Salvadores <patriciozhogan@gmail.com>
2025-10-30 22:36:55 -0300

    sin maximo la descripcopn

--- src/app/(home)/user/mis-eventos/editar/[id]/ui/UpdateEventoForm.tsx
+++ src/app/(home)/user/mis-eventos/editar/[id]/ui/UpdateEventoForm.tsx
@@ -200,7 +200,7 @@ export const UpdateEventoForm = ({
                   rows={3}
                   className='block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6'
                   minLength={5}
-                  maxLength={600}
+                  
                 />
                 <p className='mt-2 ms-2 text-sm/6 text-gray-600'>
                   Escribe una breve descripción del evento.
