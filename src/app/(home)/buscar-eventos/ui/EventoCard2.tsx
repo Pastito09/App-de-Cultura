ac77dc8b6e7a5b1bd41124ad1363d592e285d02f
Manuel Salvadores <patriciozhogan@gmail.com>
2025-10-30 21:28:28 -0300

    volver button en buscar

--- src/app/(home)/buscar-eventos/ui/EventoCard2.tsx
+++ src/app/(home)/buscar-eventos/ui/EventoCard2.tsx
@@ -42,9 +42,9 @@ export const EventoCard2 = ({
   };
 
   return (
-    <article className='flex flex-col border h-full w-full  bg-slate-50 rounded-xl px-2 pb-1'>
-      <div className='flex flex-row '>
-        <Link href={eventSlug} className='block'>
+    <Link href={eventSlug} className='block'>
+      <article className='flex flex-col border h-full w-full  bg-slate-50 rounded-xl px-2 pb-1'>
+        <div className='flex flex-row '>
           <Image
             width={150}
             height={500}
@@ -54,20 +54,20 @@ export const EventoCard2 = ({
             src={image?.url || '/placeholder.jpg'}
             className='rounded-sm pt-1 hidden sm:block w-full'
           />
-        </Link>
-        <div className='flex flex-col w-full'>
-          <div className='flex md:flex-row ms-6 mb-1 items-center justify-between gap-x-4 text-xs'>
-            <span className='font-semibold tracking-wide text-blue-900'>
-              {diaDeLaSemana || 'sin fecha'}
-            </span>
 
-            <time
-              dateTime={eventDate.toString()}
-              className='text-gray-500 text-center'
-            >
-              {dia || 'sin fecha'} - {startTime}Hs.
-            </time>
-            {/* <Link href={eventGoogleMaps || eventSlug}>
+          <div className='flex flex-col w-full'>
+            <div className='flex md:flex-row ms-6 mb-1 items-center justify-between gap-x-4 text-xs'>
+              <span className='font-semibold tracking-wide text-blue-900'>
+                {diaDeLaSemana || 'sin fecha'}
+              </span>
+
+              <time
+                dateTime={eventDate.toString()}
+                className='text-gray-500 text-center'
+              >
+                {dia || 'sin fecha'} - {startTime}Hs.
+              </time>
+              {/* <Link href={eventGoogleMaps || eventSlug}>
               <div className='flex flex-row gap-1 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100'>
                 <IoLocationSharp className='mt-0.5 hidden md:block' />
 
@@ -80,60 +80,61 @@ export const EventoCard2 = ({
                 </p>
               </div>
             </Link> */}
-            <EventLocation
-              eventLocation={eventLocation}
-              eventLocationMap={eventLocationMap}
-              eventLocationName={eventLocationName}
-            />
+              <EventLocation
+                eventLocation={eventLocation}
+                eventLocationMap={eventLocationMap}
+                eventLocationName={eventLocationName}
+              />
 
-            {tipoDeEvento(eventType)}
-          </div>
+              {tipoDeEvento(eventType)}
+            </div>
 
-          <div>
-            <Link href={eventSlug}>
-              <h3 className=' md:ms-3 sm:text-2xl font-semibold text-center md:text-start  text-gray-900 group-hover:text-gray-600'>
-                <span>
-                  <span className='inset-0' />
-                  {eventTitle}
-                </span>
-              </h3>
-              <div className='ms-5'>
-                <p className='line-clamp-2  text-sm/6 text-gray-600'>
-                  {eventDescription}
-                </p>
-              </div>
-            </Link>
-            <div className='flex gap-8 ms-5 items-center mt-2'>
-              <h2 className='text-sm font-medium text-gray-500'>
-                Entradas:
-              </h2>
-
-              <p className='text-sm m-1 text-gray-500 place-self-center'>
-                {ticketPrice !== '0' ? ` ${ticketPrice}` : '$ 0.00'}
-              </p>
-
-              {ticketLink ? (
-                <Link
-                  href={ticketLink!}
-                  target='_blank'
-                  className='bg-slate-200 ms-4 rounded-2xl p-2 hover:bg-slate-300 hover:text-cyan-900 antialiased'
-                >
-                  {ticketPrice === '0'
-                    ? 'Conseguí tus entradas'
-                    : 'Comprar'}
-                </Link>
-              ) : (
-                ticketPrice !== '0' && (
-                  <span className='text-gray-500'>
-                    Se consiguen en puerta
+            <div>
+              <Link href={eventSlug}>
+                <h3 className=' md:ms-3 sm:text-2xl font-semibold text-center md:text-start  text-gray-900 group-hover:text-gray-600'>
+                  <span>
+                    <span className='inset-0' />
+                    {eventTitle}
                   </span>
-                )
-              )}
+                </h3>
+                <div className='ms-5'>
+                  <p className='line-clamp-2  text-sm/6 text-gray-600'>
+                    {eventDescription}
+                  </p>
+                </div>
+              </Link>
+              <div className='flex gap-8 ms-5 items-center mt-2'>
+                <h2 className='text-sm font-medium text-gray-500'>
+                  Entradas:
+                </h2>
+
+                <p className='text-sm m-1 text-gray-500 place-self-center'>
+                  {ticketPrice !== '0' ? ` ${ticketPrice}` : '$ 0.00'}
+                </p>
+
+                {ticketLink ? (
+                  <Link
+                    href={ticketLink!}
+                    target='_blank'
+                    className='bg-slate-200 ms-4 rounded-2xl p-2 hover:bg-slate-300 hover:text-cyan-900 antialiased'
+                  >
+                    {ticketPrice === '0'
+                      ? 'Conseguí tus entradas'
+                      : 'Comprar'}
+                  </Link>
+                ) : (
+                  ticketPrice !== '0' && (
+                    <span className='text-gray-500'>
+                      Se consiguen en puerta
+                    </span>
+                  )
+                )}
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 };
 
