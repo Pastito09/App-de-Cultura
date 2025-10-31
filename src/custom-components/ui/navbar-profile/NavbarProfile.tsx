32fb3c59a1923fdfd50a709e167222e4d4d3fd13
Manuel Salvadores <patriciozhogan@gmail.com>
2025-10-31 12:37:12 -0300

    no mas perfil

--- src/custom-components/ui/navbar-profile/NavbarProfile.tsx
+++ src/custom-components/ui/navbar-profile/NavbarProfile.tsx
@@ -4,26 +4,26 @@ import {
   AvatarFallback,
   AvatarImage,
 } from '@/components/ui/avatar';
-import Link from 'next/link';
+// import Link from 'next/link';
 
 export const NavbarProfile = async () => {
   const session = await auth();
   const { user } = session!;
 
   return (
-    <Link
-      href={'/user/perfil'}
-      className='sm:pointer-events-none pointer-events-auto'
-    >
-      <Avatar>
-        <AvatarImage
-          src={user?.image || undefined}
-          alt='Avatar de usuario'
-        />
-        <AvatarFallback className='bg-slate-500'>
-          {user?.name?.trim().charAt(0)}
-        </AvatarFallback>
-      </Avatar>
-    </Link>
+    // <Link
+    //   href={'/user/perfil'}
+    //   className='sm:pointer-events-none pointer-events-auto'
+    // >
+    <Avatar>
+      <AvatarImage
+        src={user?.image || undefined}
+        alt='Avatar de usuario'
+      />
+      <AvatarFallback className='bg-slate-500'>
+        {user?.name?.trim().charAt(0)}
+      </AvatarFallback>
+    </Avatar>
+    // </Link>
   );
 };
